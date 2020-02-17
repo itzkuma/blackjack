7ee604ce2d4ef7b367cb0c2876595e017df7b269
itzkuma <60122679+itzkuma@users.noreply.github.com>
2020-02-17 13:11:20 -0500

    Add files via upload

---
+++ tarea dado.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace juego
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            int dado1 = 0, total = 0, acumulado = 0, turnos = 0;
+            
+            string Continuar = "s";
+
+
+
+            while (total < 100 && Continuar == "s")
+            {                
+                dado1 = aleatorio.Next(1,13);
+                total += dado1;
+                turnos++;
+                acumulado++;
+                Console.WriteLine("llevas " + turnos +" turno(s)");
+                Console.WriteLine("Dado=" +  dado1);
+                Console.WriteLine(" En Total llevas " + total + " puntos");
+                
+
+               if (turnos >3 &&  (dado1 == 1 || dado1 == 3 || dado1 ==5 || dado1 == 7 || dado1 == 9 || dado1 == 11))
+               {
+                    Continuar = "n";
+                    Console.WriteLine("Perdiste.. Ya no puedes continuar");
+                    
+               }
+
+               //else if ( dado1==12 && dado1==10)
+                //{
+                    //Console.WriteLine("GANASTE!");
+                   //Continuar = "n";
+                //}
+               else
+                {
+                    Console.WriteLine("Deseas Continuar? (s/n)");
+                    Continuar = Console.ReadLine();
+                    
+                }
+
+            }
+        }  
+    }
+}
+
+
+
+
+
+
