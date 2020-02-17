5d18458dfc1ee689bc8b20651fe400820bfb46ce
itzkuma <60122679+itzkuma@users.noreply.github.com>
2020-02-17 14:52:31 -0500

    Update tarea dado.cs

--- tarea dado.cs
+++ tarea dado.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace juego
+namespace ConsoleApp9
 {
     class Program
     {
@@ -12,49 +12,52 @@ namespace juego
         {
             Random aleatorio = new Random();
             int dado1 = 0, total = 0, acumulado = 0, turnos = 0;
-            
+
             string Continuar = "s";
+            bool condicion10 = false;
 
 
 
             while (total < 100 && Continuar == "s")
-            {                
-                dado1 = aleatorio.Next(1,13);
+            {
+                dado1 = aleatorio.Next(10, 13);
                 total += dado1;
                 turnos++;
                 acumulado++;
-                Console.WriteLine("llevas " + turnos +" turno(s)");
-                Console.WriteLine("Dado=" +  dado1);
+                Console.WriteLine("llevas " + turnos + " turno(s)");
+                Console.WriteLine("Dado=" + dado1);
                 Console.WriteLine(" En Total llevas " + total + " puntos");
-                
 
-               if (turnos >3 &&  (dado1 == 1 || dado1 == 3 || dado1 ==5 || dado1 == 7 || dado1 == 9 || dado1 == 11))
-               {
+
+                if (turnos > 3 && (dado1 == 1 || dado1 == 3 || dado1 == 5 || dado1 == 7 || dado1 == 9 || dado1 == 11))
+                {
                     Continuar = "n";
                     Console.WriteLine("Perdiste.. Ya no puedes continuar");
-                    
-               }
 
-               else if ( dado1==10)
-                {
-                   if(dado1==12)
-                   {
-                   Console.WriteLine("GANASTE!");
-                   Continuar = "n";
-                   }
-                  
                 }
-               else
+
+                if (dado1 == 10) condicion10 = true;
+                else if (dado1 != 12 && condicion10) condicion10 = false;
+                Console.WriteLine(condicion10 + " + " + dado1);
+                if ((condicion10 && dado1==12))
+                {
+                    Console.WriteLine("Ganastee!");
+                    Continuar = "n";
+                }
+               
+
+                else
                 {
                     Console.WriteLine("Deseas Continuar? (s/n)");
                     Continuar = Console.ReadLine();
-                    
+
                 }
 
             }
-        }  
+        }
     }
 }
+
 
 
 
